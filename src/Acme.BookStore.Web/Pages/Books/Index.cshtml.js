bed8fe07c22963df07407ebbdc9ca3b63212bb15
Mert <Mert@DESKTOP-83AQB57>
2024-08-05 17:46:15 +0300

    Add project files.

---
+++ src/Acme.BookStore.Web/Pages/Books/Index.cshtml.js
@@ -0,0 +1,82 @@
+$(function () {
+    var dataTable = $('#BooksTable').DataTable(
+        abp.libs.datatables.normalizeConfiguration({
+            serverSide: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: false,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(acme.bookStore.books.book.getList),
+            columnDefs: [
+                {
+                    title: 'Actions',
+                    rowAction: {
+                        items:
+                            [
+                                {
+                                    text: 'Delete',
+                                    confirmMessage: function (data) {
+                                        return 'Are you sure to delete the book: ' + data.record.name;
+                                    },
+                                    action: function (data) {
+                                        acme.bookStore.books.book
+                                            .delete(data.record.id)
+                                            .then(function () {
+                                                abp.notify.info('Successfully deleted the book');
+                                                dataTable.ajax.reload();
+                                            });
+                                    }
+                                }
+                            ]
+                    }
+                },
+                {
+                    title: 'Name',
+                    data: "name"
+                },
+                {
+                    title: 'Type',
+                    data: "type"
+                },
+                {
+                    title: 'Publish Date',
+                    data: "publishDate",
+                    render: function (data) {
+                        return luxon
+                            .DateTime
+                            .fromISO(data, {
+                                locale: abp.localization.currentCulture.name
+                            }).toLocaleString();
+                    }
+                },
+                {
+                    title: 'Price',
+                    data: "price"
+                },
+                {
+                    title: 'Creation Time', data: "creationTime",
+                    render: function (data) {
+                        return luxon
+                            .DateTime
+                            .fromISO(data, {
+                                locale: abp.localization.currentCulture.name
+                            }).toLocaleString(luxon.DateTime.DATETIME_SHORT);
+                    }
+                }
+            ]
+        })
+    );
+
+    var createModal = new abp.ModalManager(abp.appPath + 'Books/CreateModal');
+
+    $('#NewBookButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    });
+
+    createModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+   
+});
